fcf4f34a10de985c0bac22ddfc8c8816d07f1a97
Kazzbo <cedricaadam@gmail.com>
2019-02-02 16:05:06 +0100

    registration

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -13,11 +13,19 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+<<<<<<< Updated upstream
         ->add('first_name')
         ->add('last_name')
         ->add('password', PasswordType::class)
         ->add('email')
         ->add('confirm_password', PasswordType::class)
+=======
+            ->add('first_name')
+            ->add('last_name')
+            ->add('email')
+            ->add('password', PasswordType::class)
+            ->add('confirm_password', PasswordType::class)
+>>>>>>> Stashed changes
         ;
     }
 
